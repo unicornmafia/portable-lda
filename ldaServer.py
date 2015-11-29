46d2b95e8881beb496ac2df690142c3e064c1ee6
thomas <thomas@thomas.com>
2015-11-29 14:14:23 -0800

    adding fetch for document

--- ldaServer.py
+++ ldaServer.py
@@ -14,19 +14,44 @@ from datetime import timedelta
 from flask import make_response, request, current_app
 from functools import update_wrapper
 
-
-
+N = 20   # num of sims to return'
 app = Flask(__name__)
 CORS(app)
 counter = 0
 bow_vectors = None
 lda_model = None
 bow_builder = None
+cache_root = ""
 
 
 @app.route('/')
 def health():
     return 'Lda Server is totally working, you guys!'
+
+
+def build_json_list(topn):
+    retstr = '['
+    for i in range(0, N):
+        if i != 0:
+            retstr += ","
+        retstr += '{"id":%d, "name":"%s", "sim": %f}' % (i, topn[i][0], topn[i][1])
+    retstr += ']'
+    return retstr
+
+
+def read_file(fileid):
+    filepath =  os.path.join(cache_root, "plaintext", fileid)
+    try:
+        with open(filepath, "r") as datafile:
+            data=datafile.read()
+        return data
+    except:
+        return "Error Reading File: " + filepath
+
+
+@app.route('/document/<docid>', methods=['GET'])
+def get_document(docid):
+    return read_file(docid)
 
 
 @app.route('/get-term-sim', methods=['POST'])
@@ -43,13 +68,14 @@ def get_term_sim():
     vec_bow = lda_model.lda_model.id2word.doc2bow(terms)
     vec_lda = lda_model.lda_model[vec_bow]
     sims = lda_model.calc_sims_for_topic_distribution(vec_lda, sim_method)
-    topn = sims[:20]
-    topn_json = json.dumps(topn)
-    print str(topn_json)
-    return topn_json
+    topn = sims[:N]
+    json_list = build_json_list(topn)
+    # topn_json = json.dumps(topn)
+    # print str(topn_json)
+    return json_list
 
 if __name__ == '__main__':
-
+    global cache_root
     print "Loading BoW Vectors from cache...\n"
     cache_root = os.path.join(os.getcwd(), "cache")
     bow_builder = BowBuilder(cache_dir=cache_root)
